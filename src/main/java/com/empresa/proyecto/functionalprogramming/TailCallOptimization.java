7e0337507d8720ba4bf8fb356b43792429cd66b1
nerio.baez <nerio.baez.delgado@gmail.com>
2025-07-08 22:07:59 -0500

    Cambios

---
+++ src/main/java/com/empresa/proyecto/functionalprogramming/TailCallOptimization.java
@@ -0,0 +1,21 @@
+package com.empresa.proyecto.functionalprogramming;
+
+public class TailCallOptimization {
+    public static void main(String[] args) {
+
+    }
+
+    public static long reFact(int n) {
+        if (n <= 1)
+            return 1;
+        else
+            return n * reFact(n - 1);
+    }
+
+    public static long tailReFact(int n, int a) {
+        if (n <= 1)
+            return a;
+        else
+            return tailReFact(n - 1, n * a);
+    }
+}
